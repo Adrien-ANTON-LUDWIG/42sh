93f4b8165147db81f3878c85c1fe4547d668a66c
florent.tauty <florent.tauty@epita.fr>
2020-11-24 22:05:22 +0100

    Fixed the main.c file to handle basic options
    Co-authored-by: Pierre Pelletier <pierre.pelletier@epita.fr>

--- src/core/main.c
+++ src/core/main.c
@@ -6,6 +6,21 @@ int main(int argc, char const *argv[])
     if (argc < 2)
         return 0;
 
-    printf("%s\n", argv[1]);
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-c") == 0)
+        {
+            
+        }
+        else if (strcmp(argv[i], "-O") == 0)
+        {
+
+        }
+        else if (strcmp(argv[i], "+O") == 0)
+        {
+            
+        }
+
+    }
     return 0;
 }
