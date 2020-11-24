2bf5b0f486f5cfb79617102ad686e1b9204c65d6
Pierre Pelletier <pierre.pelletier@epita.fr>
2020-11-24 21:06:32 +0000

    Revert "Fixed the main.c file to handle basic options"
    This reverts commit 93f4b8165147db81f3878c85c1fe4547d668a66c

--- src/core/main.c
+++ src/core/main.c
@@ -6,21 +6,6 @@ int main(int argc, char const *argv[])
     if (argc < 2)
         return 0;
 
-    for (int i = 1; i < argc; i++)
-    {
-        if (strcmp(argv[i], "-c") == 0)
-        {
-            
-        }
-        else if (strcmp(argv[i], "-O") == 0)
-        {
-
-        }
-        else if (strcmp(argv[i], "+O") == 0)
-        {
-            
-        }
-
-    }
+    printf("%s\n", argv[1]);
     return 0;
 }
