4bc85714264158f19c7295274ba3313f8a443586
Adrien Anton Ludwig <adrien.anton-ludwig@epita.fr>
2020-11-25 22:11:27 +0100

    [ADD] struct list
    Chained list of strings
    
    Co-authored-by: Florent Tauty <florent.tauty@epita.fr>

--- src/include/my_utils.h
+++ src/include/my_utils.h
@@ -1,10 +1,12 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <ctype.h>
 #include <err.h>
+#include <stddef.h>
 #include <stdlib.h>
 
-#include "../structures.h"
+#include "structures.h"
 
 /* MAJOR */
 void major_init(void);
@@ -12,5 +14,19 @@ void major_free(struct major *mj);
 
 /* MY_ERR */
 void my_err(int err_code, struct major *mj, char *message);
+
+/* LIST */
+struct list
+{
+    struct list_item *head;
+    struct list_item *tail;
+    size_t size;
+};
+
+struct list_item
+{
+    struct list_item *next;
+    char *data;
+};
 
 #endif /* UTILS_H */
