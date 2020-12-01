9031e6b933778567976c4cd9899218a58a1328ff
florent.tauty <florent.tauty@epita.fr>
2020-12-02 00:51:38 +0100

    [MERGE] parser_cpdlist with get_next_token
    Co-authored-by: Adele Pluquet <adele.pluquet@epita.fr>

--- src/parser/parser_cpdlist.c
+++ src/parser/parser_cpdlist.c
@@ -5,7 +5,7 @@
 void parser_cpdlist(struct major *mj, struct token **expr, struct ast *newast,
                     int (*should_loop)(enum words))
 {
-    while (should_loop((*expr = lexer_build(mj))->word))
+    while (should_loop((*expr = get_next_token(mj))->word))
     {
         if ((*expr)->word == WORD_EOF)
             my_err(1, mj, "parser_if: unexpected EOF");
