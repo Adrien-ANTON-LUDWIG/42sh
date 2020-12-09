8bc800dabad90795730a98444a6bda4bfa63580e
Pierre Pelletier <pierre.pelletier@epita.fr>
2020-12-09 18:44:22 +0100

    Made parser_while clearer

--- src/parser/parser_while.c
+++ src/parser/parser_while.c
@@ -20,20 +20,25 @@ struct ast *parser_while(struct major *mj, struct ast *ast, struct token **tk)
         struct ast *newast = add_single_command(mj, ast, NULL);
         ast = newast;
     }
+
     struct ast *newast = create_ast(mj, *tk);
     *tk = get_next_token(mj);
     parser_cpdlist(mj, tk, newast, should_loop);
     newast->left = newast->right;
     newast->right = NULL;
+
     if ((*tk)->word != WORD_DO)
         my_err(2, mj, "parser_while: expected 'do'");
+
     token_free(*tk);
     *tk = get_next_token(mj);
     token_free(*tk);
     *tk = get_next_token(mj);
+
     parser_cpdlist(mj, tk, newast, should_loop);
     token_free(*tk);
     *tk = get_next_token(mj);
+
     if (ast)
         ast->right = newast;
     else
