1baf2d447881872e4c1200498daf6a71c6ff2a5d
Pierre Pelletier <pierre.pelletier@epita.fr>
2020-12-15 01:02:27 +0100

    Quotes lexer Co-authored-by: Adrien Anton-Ludwig <adrien.anton-ludwig@epita.fr>

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -91,34 +91,6 @@ static struct token *get_token_parenthesis(struct major *mj)
     return token_init(mj, WORD_LPARENTHESIS);
 }
 
-static struct token *get_token_quote(struct major *mj, char c)
-{
-    struct custom_FILE *f = mj->file;
-
-    size_t start = f->lexer_index - 1;
-    char quote = c;
-    char lastchar = '\\';
-
-    while (c != quote || lastchar == '\\')
-    {
-        lastchar = quote;
-        c = get_char(f, 1);
-
-        if (at_end(f) && c != '\"')
-            if (!custom_getline_same_buf(mj))
-                my_err(2, mj, "get_token_quote: unexpected EOF");
-    }
-
-    size_t end = f->lexer_index;
-    size_t len = end - start;
-
-    struct token *tk = token_init(mj, WORD_WORD);
-    char *s = strndup(f->str + start, len);
-    tk->data = list_append(mj, tk->data, s);
-
-    return tk;
-}
-
 static struct token *get_token_operator(struct major *mj)
 {
     struct custom_FILE *f = mj->file;
@@ -136,8 +108,6 @@ static struct token *get_token_operator(struct major *mj)
         return get_token_parenthesis(mj);
     if (c == ')')
         return token_init(mj, WORD_RPARENTHESIS);
-    if (c == '"' || c == '\'')
-        return get_token_quote(mj, c);
     return get_token_redir(mj, c);
 }
 
@@ -158,19 +128,49 @@ static struct token *get_token_operator_skip_newline(struct major *mj)
 
     return tk;
 }
+
+static void skip_quote(struct major *mj, char c)
+{
+    struct custom_FILE *f = mj->file;
+
+    if (f->lexer_index > 0 && f->str[f->lexer_index - 1] == '\\')
+    {
+        f->lexer_index++;
+        return;
+    }
+
+    char quote = c;
+    char lastchar = c;
+    c = get_char(f, 1);
+
+    while (c != quote || (quote == '"' && lastchar == '\\'))
+    {
+        lastchar = c;
+        c = get_char(f, 1);
+
+        if (at_end(f) && c != quote)
+            if (!custom_getline_same_buf(mj))
+                my_err(2, mj, "get_token_quote: unexpected EOF");
+    }
+}
+
+// /!\ 25 lignes
 static struct token *get_token_word(struct major *mj)
 {
     struct custom_FILE *f = mj->file;
     size_t start = f->lexer_index;
-    char c = '"';
+    char c = get_char(f, 0);
 
-    while (is_not_in(get_char(f, 0), IS_NOT_WORD) && (c == '"' || c == '\''))
+    while (!at_end(f) && is_not_in(c, IS_NOT_WORD))
     {
-        skip(f, is_not_in, IS_NOT_WORD);
-        c = get_char(f, 0);
-
-        if ((c == '"' || c == '\'') && f->str[f->lexer_index - 1] == '\\')
+        if (is_in(c, "'\""))
+        {
             f->lexer_index++;
+            skip_quote(mj, c);
+        }
+        else
+            skip(f, is_not_in, IS_NOT_WORD_OR_QUOTE);
+        c = get_char(f, 0);
     }
 
     size_t end = f->lexer_index;
@@ -198,6 +198,25 @@ static struct token *get_token_word(struct major *mj)
     return tk;
 }
 
+static void set_if_assignment(struct token *tk)
+{
+    char *s = tk->data->head->data;
+
+    if ('0' <= *s && *s <= '9')
+        return;
+
+    char *equal = strstr(s, "=");
+
+    if (!equal || s == equal)
+        return;
+
+    for (; s != equal; s++)
+        if (!(isalnum(*s) || *s == '_'))
+            return;
+
+    tk->word = WORD_ASSIGNMENT;
+}
+
 struct token *get_next_token(struct major *mj)
 {
     struct custom_FILE *f = mj->file;
@@ -223,5 +242,8 @@ struct token *get_next_token(struct major *mj)
     if (is_in(get_char(f, 0), IS_OPERATOR))
         return get_token_operator_skip_newline(mj);
 
-    return get_token_word(mj);
+    struct token *word = get_token_word(mj);
+    set_if_assignment(word);
+
+    return word;
 }
