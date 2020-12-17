4d5b94dff8e74a4fe502be5325a9350c7ec8d725
Adrien Anton Ludwig <adrien.anton-ludwig@epita.fr>
2020-12-17 18:58:45 +0100

    [ADD] Tokens case and esac
    Co-authored-by: Pierre Pelletier <pierre.pelletier@epita.fr>

--- src/tokens/tokens.c
+++ src/tokens/tokens.c
@@ -8,20 +8,6 @@
 #include "my_err.h"
 #include "my_xmalloc.h"
 
-#define TOKENS_TO_STRING                                                       \
-    {                                                                          \
-        "WORD_IF", "WORD_THEN", "WORD_ELIF", "WORD_ELSE", "WORD_FI",           \
-            "WORD_WHILE", "WORD_UNTIL", "WORD_FOR", "WORD_IN", "WORD_DO",      \
-            "WORD_DONE", "WORD_FUNCTION", "WORD_LBRACKET", "WORD_RBRACKET",    \
-            "WORD_AND", "WORD_OR", "WORD_PIPE", "WORD_REDIR_LR",               \
-            "WORD_REDIR_RP", "WORD_REDIR_LA", "WORD_REDIR_RA",                 \
-            "WORD_REDIR_LLM", "WORD_REDIR_LL", "WORD_REDIR_RR",                \
-            "WORD_REDIR_L", "WORD_REDIR_R", "WORD_COMMAND", "WORD_EOF",        \
-            "WORD_SUPERAND", "WORD_SEMIC", "WORD_NEWLINE",                     \
-            "WORD_LPARENTHESIS", "WORD_RPARENTHESIS", "WORD_DPARENTHESIS",     \
-            "WORD_WORD", "WORD_ASSIGNMENT", "WORD_DSEMIC"                      \
-    }
-
 struct token *token_init(struct major *mj, enum words word)
 {
     struct token *new = my_xcalloc(mj, 1, sizeof(struct token));
